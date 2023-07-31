fa38b87c8923fb588193da4a3f9602cc50761b51
mateta kiese <israelmateta685@gmail.com>
2023-07-31 10:37:15 +0100

    feat: messages aleatoires

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 //helloh
-switch(aleatoire){
-    case 0: message("La première étape est de dire que tu peux.")
-    case 1: message("Si tu traverses l’enfer, continue.")
-    case 2: message("“Impossible” est pour ceux qui ne veulent pas vraiment.")
-    case 3: message("Essaye encore. Échoue de nouveau. Échoue mieux.")
-    case 4: message("Le fou se questionne, le sage demande.")
-    case 5: message("Rêve sans peur. Aime sans limite.")
-    case 6: message("Pas de pression, pas de diamants.")
-}
-aleatoire = Math.floor(Math.random()*7)
+function messageAre(array){
 
-console.log(aleatoire);
+    const rand = Math.random()*array.length | 0;
+    const rValue = array[rand];
+    return rValue;
+    
+
+}
+
+const area = ["La première étape est de dire que tu peux.", "Si tu traverses l’enfer, continue.", "“Impossible” est pour ceux qui ne veulent pas vraiment.",  "Essaye encore. Échoue de nouveau. Échoue mieux.", "Le fou se questionne, le sage demande.", "Rêve sans peur. Aime sans limite.", "Pas de pression, pas de diamants."]
+const rValue = messageAre(area);
+
+console.log(rValue)
+
+
+
+
+
+
+
+
+
