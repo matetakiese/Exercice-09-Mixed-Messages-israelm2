83239848c6fde3ebf148a6a4984ffb9491e3e697
mateta kiese <israelmateta685@gmail.com>
2023-07-31 08:27:49 +0100

    message

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//hello
+//helloh
+switch(aleatoire){
+    case 0: message("La première étape est de dire que tu peux.")
+    case 1: message("Si tu traverses l’enfer, continue.")
+    case 2: message("“Impossible” est pour ceux qui ne veulent pas vraiment.")
+    case 3: message("Essaye encore. Échoue de nouveau. Échoue mieux.")
+    case 4: message("Le fou se questionne, le sage demande.")
+    case 5: message("Rêve sans peur. Aime sans limite.")
+    case 6: message("Pas de pression, pas de diamants.")
+}
+aleatoire = Math.floor(Math.random()*7)
+
+console.log(aleatoire);
